b6607a2feb424fb84de7d083c70b2555a402b826
iamtruck <ee1230532@iitd.ac.in>
2026-01-31 23:26:29 +0530

    test4

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const messages = [
 
 
 function update() {
-  const now = new Date("2026-02-1T00:00:00");
+  const now = new Date("2026-02-01T00:00:00");
   const diff = valentines - now;
 
   // If Valentine’s has passed
